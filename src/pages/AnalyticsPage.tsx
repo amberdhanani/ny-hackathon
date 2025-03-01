32cb829d342327eacb61200d9cebb1c0a9eac8a6
Amber Dhanani <amberdhanani@Ambers-Air.attlocal.net>
2025-03-01 15:04:39 -0600

    analytics graphs in

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from '../NavBar'
+import AnalyticsContainer from '../components/analytics/AnalyticsContainer'
 
 const AnalyticsPage = () => {
   return (
     <div><NavBar title="Analytics" />
+    <AnalyticsContainer />
     </div>
   )
 }

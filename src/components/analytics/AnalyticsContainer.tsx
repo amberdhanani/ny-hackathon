67b52419bd092510c2bb756681585b2c3b88d907
Amber Dhanani <amberdhanani@Ambers-Air.attlocal.net>
2025-03-01 13:01:12 -0600

    remove DL button

---
+++ src/components/analytics/AnalyticsContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnalyticsContainer = () => {
+  return (
+    <div>AnalyticsContainer</div>
+  )
+}
+
+export default AnalyticsContainer
--- src/components/newRecordings/NewRecordingContainer.tsx
+++ src/components/newRecordings/NewRecordingContainer.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
+import Lottie from "lottie-react";
+import recordingAnimation from "../../assets/recording.json"; // Ensure the correct path
+
 import { selectedTranscriptAtom, transcriptsAtom } from "../../recoil/atoms";
 import { useAudioRecorder } from "../../hooks/useAudioRecorder";
 import { transcribeAudio } from "../../utils/transcribeAudio";
@@ -43,12 +46,24 @@ const NewRecordingContainer: React.FC = () => {
   };
 
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      {isRecording && (
+        <Lottie
+          animationData={recordingAnimation}
+          loop
+          style={{ width: 150, height: 150 }}
+        />
+      )}
+      
       {isRecording && <h3>Recording...</h3>}
       {isTranscribing && <p>Transcribing...</p>}
 
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button onClick={isRecording ? handleStopRecording : startRecording}>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+        <Button
+          variant="contained"
+          color={isRecording ? "secondary" : "primary"}
+          onClick={isRecording ? handleStopRecording : startRecording}
+        >
           {isRecording ? "Stop Recording" : "Start Recording"}
         </Button>
       </div>
